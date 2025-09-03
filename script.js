b7a722a8588a9a842f77faca15b0ecf339615008
frank louis <frankplouis7@gmail.com>
2025-09-03 20:44:34 +0530

    Neural network working

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ canvas.width=200
 
 const ctx = canvas.getContext("2d"); // we  are giving access to ctx of all the 2d drawing tools
 const road = new Road(canvas.width/2,canvas.width*0.9)
-const car = new Car(road.getLanecenter(1), 100, 30, 50, "KEYS");
+const car = new Car(road.getLanecenter(1), 100, 30, 50, "AI");
 const traffic = [
     new Car(road.getLanecenter(1), -300, 30, 50, "DUMMY", 2), 
 ];
